3941c4d0a3d14beaaaeeffcbd8832a9659e9c8e3
tarangdave <tarangadave@gmail.com>
2019-11-13 16:17:05 -0800

    push gh-pages

---
+++ precache-manifest.8ec6a9831c6ee8336e000fa1e44db22d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce890c3546bd7aa1d06cdfff86c004a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5f46515608880ad237b",
+    "url": "/static/css/main.b609afaf.chunk.css"
+  },
+  {
+    "revision": "c50a4ed74e6ab7388c55",
+    "url": "/static/js/2.2649d27b.chunk.js"
+  },
+  {
+    "revision": "c5f46515608880ad237b",
+    "url": "/static/js/main.0e33695a.chunk.js"
+  },
+  {
+    "revision": "b630213e71f2e4780836",
+    "url": "/static/js/runtime-main.4400218f.js"
+  }
+]);
